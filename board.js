2fad43424ecc2033cfd320464b11ae2fdad4312d
nadine_b <nadine.bisswang@hpe.com>
2019-10-03 21:58:04 +0200

    Input-Feld mit Hinzufügen-Funktion (JS) und "Löschen"-Funktion (JS) erstellt, jedoch passt die Ausrichtung der Elemente noch nicht ganz

--- board.js
+++ board.js
@@ -1,14 +1,37 @@
+//Close-Icon hinter jedem List-Item
+var todoList = document.getElementsByTagName("li");
+var i;
+for (i = 0; i < todoList.length; i++) {
+    var span = document.createElement("SPAN");
+    //u007D7 macht ein x hinter das Listenelement, um diesen zu löschen/ schließen
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    todoList[i].appendChild(span);
+}
+
+// Beim Klicken auf das Close-Icon wird das List-Item versteckt
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+
+// Create a new list item when clicking on the "Add" button
 function newItem() {
     var li = document.createElement("li");
-    var inputValue = document.getElementById("myInput").value;
-    var t = document.createTextNode(inputValue);
+    var inputWert = document.getElementById("input_ToDo").value;
+    var t = document.createTextNode(inputWert);
     li.appendChild(t);
-    if (inputValue === '') {
-        alert("You must write something!");
+    if (inputWert === '') {
+        alert("Ungültige Eingabe! Bitte ToDo eingeben!");
     } else {
-        document.getElementById("myUL").appendChild(li);
+        document.getElementById("meineListe").appendChild(li);
     }
-    document.getElementById("myInput").value = "";
+    document.getElementById("input_ToDo").value = "";
 
     var span = document.createElement("SPAN");
     var txt = document.createTextNode("\u00D7");
